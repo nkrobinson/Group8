74ed6f97074d2f2f4ce737cc4665de8bcca576e6
Nicholas Robinson <nr124@hw.ac.uk>
2016-02-26 13:17:33 +0000

    Default Config File

--- Website/Jacob/test/config.php
+++ Website/Jacob/test/config.php
@@ -2,10 +2,10 @@
 // Copyright Group 8 Heriot-Watt University, 2016
 
 //Mysql stuff
-$db_host = "localhost";
-$db_user = "tpadmin_hwu";
-$db_db = "tpadmin_hwu";
-$db_pass = "@rlcgqeS?[+x";
+$db_host = "";
+$db_user = "";
+$db_db = "";
+$db_pass = "";
 
 date_default_timezone_set('Europe/London');
 ?>
