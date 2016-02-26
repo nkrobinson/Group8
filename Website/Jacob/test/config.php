2789c79b2f93c47cd63aea56f77ee32328d62b83
Nicholas Robinson <nr124@hw.ac.uk>
2016-02-26 13:25:55 +0000

    Testing config.php and .gitignore

--- Website/Jacob/test/config.php
+++ Website/Jacob/test/config.php
@@ -2,10 +2,10 @@
 // Copyright Group 8 Heriot-Watt University, 2016
 
 //Mysql stuff
-$db_host = "";
-$db_user = "";
-$db_db = "";
-$db_pass = "";
+$db_host = "TEST";
+$db_user = "TEST";
+$db_db = "TEST";
+$db_pass = "TEST";
 
 date_default_timezone_set('Europe/London');
 ?>
